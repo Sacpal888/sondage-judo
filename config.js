5c4273dad9858e16ed714db2cfd41488008f2a1c
GOLIVE <pascal@ocelo.be>
2025-10-08 09:34:30 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@
 // 3. Copiez "Project URL" et "anon public" key
 // 4. Remplacez les valeurs ci-dessous
 
-const SUPABASE_URL = 'https://VOTRE-PROJET.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvdHJlLXByb2pldCIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNjg5MDAwMDAwLCJleHAiOjE4NDY3ODAwMDB9.VOTRE-CLE-COMPLETE-ICI';
+const SUPABASE_URL = 'https://mtgwnqzobxvnducxpuww.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10Z3ducXpvYnh2bmR1Y3hwdXd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4ODM5MjQsImV4cCI6MjA3NTQ1OTkyNH0.iB6xGRvZsA5Dcjq0vTfoUB1Rhk6cW7HV4yF9-yew8DM';
 
 // ⚠️ IMPORTANT :
 // - Ne partagez JAMAIS ces clés publiquement
