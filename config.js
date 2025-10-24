fed04effca6d3b1b0f9fb11669fba9b33be1768d
GOLIVE <pascal@ocelo.be>
2025-10-24 18:29:04 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // CONFIGURATION SUPABASE
 // ========================================
 
-const SUPABASE_URL = 'https://mtgwnqzobxvnducxpuww.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10Z3ducXpvYnh2bmR1Y3hwdXd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4ODM5MjQsImV4cCI6MjA3NTQ1OTkyNH0.iB6xGRvZsA5Dcjq0vTfoUB1Rhk6cW7HV4yF9-yew8DM';
+window.SUPABASE_URL = 'https://mtgwnqzobxvnducxpuww.supabase.co';
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10Z3ducXpvYnh2bmR1Y3hwdXd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4ODM5MjQsImV4cCI6MjA3NTQ1OTkyNH0.iB6xGRvZsA5Dcjq0vTfoUB1Rhk6cW7HV4yF9-yew8DM';
 
 // Configuration complète et prête à l'emploi ✅
